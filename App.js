355bdcf0b8bb6f1f27c6021076bcc050424aec0b
joban <91016674+acrcw@users.noreply.github.com>
2023-11-18 17:27:12 +0530

    done

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Tabs from "./src/components/Tabs";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View,Text } from "react-native";
 import { useGetWeather } from "./src/hooks/useGetWeather";
 
 const App = () => {
   const [loading, error, weather] = useGetWeather();
   
   if (loading) {
-    
     return (
+      // <View style={[styles.container, styles.horizontal]}>
+      //   <ActivityIndicator size='large' color='#0000ff' />
+      // </View>
       <View style={[styles.container, styles.horizontal]}>
-        <ActivityIndicator size='large' color='#0000ff' />
+        <Text> Loading </Text>
       </View>
     );
   } else {
